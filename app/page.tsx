e7fc08673a8b8d8b4dc554eccce38f4088551250
elinoza <hillcakmak@gmail.com>
2024-06-11 02:01:18 +0300

    github url changed

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex relative min-h-screen bg-gradient-to-t from-[#E4E7EB] to-[#ECE9E3] flex-col items-center justify-center p-24">
       <span className="absolute top-5 right-10 text-2xl">
-        <a href="https://github.com/elinoza/tadaa-App" target="_blank">
+        <a href="https://github.com/elinoza/calculator" target="_blank">
           <IoLogoGithub />
         </a>
       </span>
