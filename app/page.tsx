07617d14789b248487b0c7339fc944139e0ce4ba
elinoza <hillcakmak@gmail.com>
2024-06-11 01:56:46 +0300

    github icon added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 import Calculator from "../components/Calculator";
+import { IoLogoGithub } from "react-icons/io";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-gradient-to-t from-[#E4E7EB] to-[#ECE9E3] flex-col items-center justify-center p-24">
+    <main className="flex relative min-h-screen bg-gradient-to-t from-[#E4E7EB] to-[#ECE9E3] flex-col items-center justify-center p-24">
+      <span className="absolute top-5 right-10 text-2xl">
+        <a href="https://github.com/elinoza/tadaa-App" target="_blank">
+          <IoLogoGithub />
+        </a>
+      </span>
       <Calculator />
     </main>
   );
